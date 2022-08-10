3b509eaf579c1445e57ac8926735d0445783d83b
bahtierkamalov21 <bahtierkamalov21@gmail.com>
2022-08-10 16:06:51 +0600

    backup

--- our_projects/settings.py
+++ our_projects/settings.py
@@ -83,23 +83,23 @@ WSGI_APPLICATION = 'our_projects.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'our_projects_db',
-#         'USER': 'root',
-#         'PASSWORD': 'ba81906377',
-#         'HOST': '127.0.0.1',  # Or an IP Address that your DB is hosted on
-#         'PORT': '3306',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'our_projects_database',
+        'USER': 'root',
+        'PASSWORD': 'ba81906377',
+        'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
